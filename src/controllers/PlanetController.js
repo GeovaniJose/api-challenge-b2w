ebae6985410398c3174cc06671c888a3ef1ca50e
Geovani José <geovani-lol@hotmail.com>
2018-12-05 00:00:43 -0200

    Add PlanetController and api

---
+++ src/controllers/PlanetController.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose')
+
+const api = require('../services/api')
+
+const Planet = mongoose.model('Planet')
+
+module.exports = {
+  async add (req, res) {
+    try {
+      const arrayPlanet = await api.get(`planets/?search=${req.body.name}`)
+
+      const arrayFilms = arrayPlanet.results[0]
+
+      const films = arrayFilms.films.length()
+
+      req.body = { films, ...req.body }
+
+      const planet = await Planet.create(req.body)
+
+      return res.status(201).json(planet)
+    } catch (err) {
+      return res.status(400).send('There was an error adding the planets.')
+    }
+  },
+
+  async list (req, res) {
+    try {
+      const { page = 1 } = req.query
+
+      const planets = await Planet.paginate({}, { page, limit: 10 })
+
+      return res.status(200).json(planets)
+    } catch (err) {
+      return res.status(400).send('There was an error retrieving the planets.')
+    }
+  },
+
+  async readByName (req, res) {
+    try {
+      const planet = await Planet.find({ name: req.params.id })
+
+      return res.status(200).json(planet)
+    } catch (err) {
+      return res.status(400).send('There was an error retrieving the planet by name.')
+    }
+  },
+
+  async readById (req, res) {
+    try {
+      const planet = await Planet.findById(req.params.id)
+
+      return res.status(200).json(planet)
+    } catch (err) {
+      return res.status(400).send('There was an error retrieving the planet by id.')
+    }
+  },
+
+  async destroy (req, res) {
+    try {
+      await Planet.findByIdAndRemove(req.params.id)
+
+      return res.status(200).send('Planet successfully removed')
+    } catch (err) {
+      return res.status(400).send('There was an error removing the planet.')
+    }
+  }
+}
