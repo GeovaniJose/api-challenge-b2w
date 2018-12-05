60588c84e67708ecc8a5efecd84db0dfadd4fc77
Geovani José <geovani-lol@hotmail.com>
2018-12-05 00:06:03 -0200

    Add routes

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const routes = express.Router()
+
+const PlanetController = require('./controllers/PlanetController')
+
+routes.post('/planets', PlanetController.add)
+routes.get('/planets', PlanetController.list)
+routes.get('/planets/:name', PlanetController.readByName)
+routes.get('/planets/:id', PlanetController.readById)
+routes.delete('/planets/:id', PlanetController.destroy)
+
+module.exports = routes
